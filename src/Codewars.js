25cc75aea719787bc038fe399596253bfec180b3
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2022-12-25 00:13:58 +0300

    add function solution

--- src/Codewars.js
+++ src/Codewars.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 function Codewars(props) {
+
+    function solution(string) {
+        const array = string.split('');
+        let newarr = '';
+
+          array.map(item => {
+            if(item.toUpperCase() == item){
+               return  newarr =  newarr + " " + item
+            }else {
+                return newarr = newarr + item
+
+            }
+        })
+
+    }
+
+    solution('stЕing')
+
     return (
+
+
+
         <div></div>
     );
 }
