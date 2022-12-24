c64b6fe900dce86eb1c22c7d198841d997b08c5e
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2022-12-24 20:41:37 +0300

    delete test

---
+++ src/Codewars.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Codewars(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default Codewars;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Codewars from "./Codewars";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Codewars />
   </React.StrictMode>
 );
