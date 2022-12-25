3bf2f6320b11f5022d067054f6efedc7237dd88e
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2022-12-25 10:37:19 +0300

    fixed array

--- src/Codewars.js
+++ src/Codewars.js
@@ -2,25 +2,34 @@ import React from 'react';
 
 function Codewars(props) {
 
-    function solution(string) {
-        const array = string.split('');
-        let newarr = '';
+    /*const value2 = [1, 2, 3, 1, 2, 1, 2, 3, 2];
+    const value3 = value2.filter((item, i) => item == 2).length;
+    console.log("ddd", value3)*/
 
-          array.map(item => {
-            if(item.toUpperCase() == item){
-               return  newarr =  newarr + " " + item
-            }else {
-                return newarr = newarr + item
+    function deleteNth(arr, n) {
+
+        const array = [];
+        for (let i = 0; i < arr.length; i++) {
+
+             let value = arr.filter(item => item ==  array[i]).length;
+
+            console.log("sss", value);
+
+            if (value < n) {
+                array.push(arr[i]);
 
             }
-        })
 
-    }
+        }
+        console.log(array)
 
-    solution('stЕing')
+        }
+
+
+    deleteNth([4, 1, 2, 3, 1, 2, 1, 2, 3, 2], 2);
+
 
     return (
-
 
 
         <div></div>
